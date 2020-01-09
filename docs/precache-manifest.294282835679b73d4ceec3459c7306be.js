c26432e9e236bf35b47ff692d63743f92a06ce84
Akira Kobori <private.beats@gmail.com>
2020-01-10 02:38:15 +0900

    release

---
+++ docs/precache-manifest.294282835679b73d4ceec3459c7306be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deac56deb86459f47fe5",
+    "url": "css/app.578ae515.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "1075d8823ee1ffcc43d850c898d3f784",
+    "url": "index.html"
+  },
+  {
+    "revision": "deac56deb86459f47fe5",
+    "url": "js/app.c62dd23f.js"
+  },
+  {
+    "revision": "5e727348b2da7f553c6c",
+    "url": "js/chunk-vendors.b6b2dd8c.js"
+  },
+  {
+    "revision": "67236affc672939503bf33d02e4505ea",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
